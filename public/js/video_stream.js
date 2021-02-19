ed05229ecf9cd58ba9ceb12cf4c04915ab0784ec
kauli-sabino <kaulisabino27@gmail.com>
2021-02-18 22:04:08 -0300

    new stuns servers

--- public/js/video_stream.js
+++ public/js/video_stream.js
@@ -4,7 +4,31 @@ const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer({
   config: {'iceServers': [
     { url: 'stun:stun.l.google.com:19302' },
-    { url: 'turn:homeo@turn.bistri.com:80', credential: 'homeo' }
+
+{url: "stun.12connect.com:3478"},
+{url: "stun.12voip.com:3478"},
+{url: "stun.1und1.de:3478"},
+{url: "stun.2talk.co.nz:3478"},
+{url: "stun.2talk.com:3478"},
+{url: "stun.3clogic.com:3478"},
+{url: "stun.3cx.com:3478"},
+{url: "stun.a-mm.tv:3478"},
+{url: "stun.aa.net.uk:3478"},
+{url: "stun.acrobits.cz:3478"},
+{url: "stun.actionvoip.com:3478"},
+{url: "stun.advfn.com:3478"},
+{url: "stun.aeta-audio.com:3478"},
+{url: "stun.aeta.com:3478"},
+{url: "stun.alltel.com.au:3478"},
+{url: "stun.altar.com.pl:3478"},
+{url: "stun.annatel.net:3478"},
+{url: "stun.antisip.com:3478"},
+{url: "stun.arbuz.ru:3478"},
+{url: "stun.avigora.com:3478"},
+{url: "stun.avigora.fr:3478"},
+{url: "stun.awa-shima.com:3478"},
+{url: "stun.awt.be:3478"},
+{url: "stun.b2b2c.ca:3478"},
   ]} /* Sample servers, please use appropriate ones */
 })
 const myVideo = document.createElement('video')
